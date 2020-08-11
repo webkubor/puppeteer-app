87c84b6eff2f56dfef23d4f8517b7010a396f8f6
webkubor <webkubor@163.com>
2020-08-11 13:37:19 +0800

    提交本地文件

--- pdf.js
+++ pdf.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await  puppeteer.launch() 
   const page = await browser.newPage();
-  await page.goto('http://localhost:8080', {waitUntil: 'networkidle2'});
+  await page.goto('https://web-kubor.gitee.io/resume', {waitUntil: 'networkidle2'});
 
   await page.emulateMedia('screen');
   await page.pdf({path: 'webkubor.pdf', format: 'A4',scale: 0.7,margin:{top: '20px',left: '20px',bottom: '20px',right: '20px'}}); // create a PDF
--- screenshot.js
+++ screenshot.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 puppeteer.launch().then(async browser => {
   const page = await browser.newPage();
   console.log('获取网页中..........')
-  await page.goto('https://wangenbo.cn', {
+  await page.goto('https://web-kubor.gitee.io/resume', {
     timeout: 0 //传0则为无限等待，直到加载渲染完毕
   });
   /** 
