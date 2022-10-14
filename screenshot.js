71602c9529e571b482de39f32dc9744cf752cab0
webkubor <webkubor@163.com>
2022-10-14 17:12:31 +0800

    feat  移除注释

--- screenshot.js
+++ screenshot.js
@@ -1,4 +1,3 @@
-// 获取网页截图
 const puppeteer = require("puppeteer");
 puppeteer.launch().then(async (browser) => {
   const page = await browser.newPage();
@@ -7,12 +6,7 @@ puppeteer.launch().then(async (browser) => {
   await page.goto("https://shorter.finance", {
     timeout: 0, //传0则为无限等待，直到加载渲染完毕
   });
-  /**
-   * screenshot
-   * path 路径
-   * fullPage 全部图片
-   * omitBackground 不允许透明截图
-   */
+ 
   console.log("网站渲染完毕....");
   await page.screenshot({
     path: "screenshot.png",
