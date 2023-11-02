12c793b37394c9664285dcbfb7f0bff04362b8dd
webkubor <webkubor@163.com>
2023-11-02 16:34:00 +0800

    feat 新增导出形式

--- short/index.mjs
+++ short/index.mjs
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+import puppeteer  from "puppeteer"
 
 async function captureScreenshot(url, outputFilePath) {
   const browser = await puppeteer.launch({headless: true});
