5cbac77cb7cbdbb17d4f2ededbe475c1025fba56
webkubor <webkubor@163.com>
2024-09-09 17:52:32 +0800

    Feat 提交测试环境

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import chalk from 'chalk';
+import { fileURLToPath } from 'node:url'
+import { createServer } from 'vite'
+
 
 
 function outPrint(obj) {
@@ -11,6 +14,20 @@ function _judgeTypes(target) {
     return Object.prototype.toString.call(target).replace(/^\[object\s(\w+)\]$/, '$1').toLowerCase()
     //return Reflect.apply(Object.prototype.toString, target, []).replace(/^\[object\s(\w+)\]$/, '$1').toLowerCase()
 }
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+const server = await createServer({
+  // 任何合法的用户配置选项，加上 `mode` 和 `configFile`
+  configFile: false,
+  root: __dirname,
+  server: {
+    port: 1337,
+  },
+})
+await server.listen()
+
+server.printUrls()
+server.bindCLIShortcuts({ print: true })
 
 
 console.log(`output->outPrint`, outPrint('初始化'))
